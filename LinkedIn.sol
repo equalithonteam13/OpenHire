17b5cff1ef2f2972d26b519c320f3ff27aacf0b4
MistuhMok <47574763+MistuhMok@users.noreply.github.com>
2019-07-30 03:42:11 -0400

    feat: tier 1
    completed tier 1 features

--- LinkedIn.sol
+++ LinkedIn.sol
@@ -1,10 +1,8 @@
-    
 pragma solidity ^0.5.10;
 pragma experimental ABIEncoderV2;
 
 contract Administrator {
     address private adminAddress;
-    
     
     constructor() internal {
         adminAddress = msg.sender;
@@ -23,11 +21,39 @@ contract Administrator {
 
 contract LinkedIn is Administrator {
     mapping(address => Organization) allOrganizations;
+    mapping(address => User) allUsers;
+    address[] public allUserAddresses;
+    
     
     struct Organization {
         string name;
         string emailAddress;
         bool verified;
+    }
+    
+    //Can not return structs with mappings
+    struct User {
+        string name;
+        string emailAddress;
+        address[] experienceKeys;
+        //mapping so that organizations can look themselves up and approve
+        mapping(address => Experience) experience;
+        
+        //Can have either array of structs or mapping skill name to array + array with keys        
+        Skill[] skillsList;
+        
+    }
+    
+    struct Experience {
+        string organization;
+        string expertise;
+        uint duration;
+        bool verified;
+    }
+    
+    struct Skill {
+        string name;
+        address[] endorsers;
     }
     
     function createOrganization(string memory name, string memory emailAddress) public {
@@ -46,5 +72,52 @@ contract LinkedIn is Administrator {
     
     function verifyOrganization(address organizationAddress) public onlyAdmin {
         allOrganizations[organizationAddress].verified = true;
+    }
+    
+    //Differentiate user from organization while creating on the front end
+    function createUser(string memory name, string memory emailAddress) public {
+        User storage newUser = allUsers[msg.sender];
+        newUser.name = name;
+        newUser.emailAddress = emailAddress;
+        
+        allUserAddresses.push(msg.sender);
+    }
+    
+    function addExperience(address organizationAddress, string memory organizationName, string memory expertise, uint duration) public {
+        Experience memory newExperience = Experience({
+            organization: organizationName,
+            expertise: expertise,
+            duration: duration,
+            verified: false
+        });
+        
+        allUsers[msg.sender].experienceKeys.push(organizationAddress);
+        allUsers[msg.sender].experience[organizationAddress] = newExperience;
+    }
+    
+    //Organizations verify User experience
+    function verifyExperience(address userAddress) public {
+        require(allOrganizations[msg.sender].verified == true);
+        allUsers[userAddress].experience[msg.sender].verified = true;
+    }
+    
+    function addSkill(string memory skillName) public {
+        Skill memory newSkill = Skill({
+            name: skillName,
+            endorsers: new address[](0)
+        });
+        allUsers[msg.sender].skillsList.push(newSkill);
+    }
+    
+    function endorseSkill(address endorseeAddress, uint index) public {
+        allUsers[endorseeAddress].skillsList[index].endorsers.push(msg.sender);
+    }
+    
+    function getUserData(address userAddress) public view returns (string memory, string memory, address[] memory, Skill[] memory) {
+        return (allUsers[userAddress].name, allUsers[userAddress].emailAddress, allUsers[userAddress].experienceKeys, allUsers[userAddress].skillsList);
+    }
+    
+    function getExperience(address userAddress, address organizationAddress) public view returns (string memory, string memory, uint, bool) {
+        return (allUsers[userAddress].experience[organizationAddress].organization,allUsers[userAddress].experience[organizationAddress].expertise,allUsers[userAddress].experience[organizationAddress].duration,allUsers[userAddress].experience[organizationAddress].verified);
     }
 }
