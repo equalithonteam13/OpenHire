984c42f5bb33fd7038b69e0ac863c0ba229d2046
Michael Ng <ng.michael.m@gmail.com>
2019-08-08 13:16:37 -0400

    worked on rendering current employees

--- openhire/src/OrganizationComponents/OrgCurrentEmployeesComponent.js
+++ openhire/src/OrganizationComponents/OrgCurrentEmployeesComponent.js
@@ -1,66 +1,97 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
 
 export default class OrgCurrentEmployeesComponent extends Component {
-    constructor() {
-        super()
-        this.state = {}
-    }
-    async componentDidMount() {
-        await this.fetchCurrentEmployees();
-    }
+  constructor() {
+    super();
+    this.state = {};
+  }
+  async componentDidMount() {
+    await this.fetchCurrentEmployees();
+  }
 
-    async fetchCurrentEmployees() {
-        const { drizzle } = this.props;
+  async fetchCurrentEmployees() {
+    const { drizzle } = this.props;
 
-        const currentLoggedInAddressArray = await drizzle.web3.eth.getAccounts()
-        const currentLoggedInAddress = currentLoggedInAddressArray[0]
-        console.log(currentLoggedInAddress)
+    // TODO: fix this!!! to current account
+    const currentLoggedInAddressArray = await drizzle.web3.eth.getAccounts();
+    const currentLoggedInAddress = currentLoggedInAddressArray[0];
+    console.log(currentLoggedInAddress);
 
-        // console.log(drizzle.contracts.OpenHire.methods)
-        const userAddressesArrayLength = await drizzle.contracts.OpenHire.methods.getAllUsersLength().call();
+    // console.log(drizzle.contracts.OpenHire.methods)
+    const userAddressesArrayLength = await drizzle.contracts.OpenHire.methods
+      .getAllUsersLength()
+      .call();
 
-        const CURRENT_EMPLOYEES = [];
-        for (let index = 0; index < userAddressesArrayLength; index++) {
-            const userAddress = await drizzle.contracts.OpenHire.methods.allUserAddresses(index).call();
-            const userStruct = await drizzle.contracts.OpenHire.methods.getUserData(userAddress).call();
+    const CURRENT_EMPLOYEES = [];
+    for (let index = 0; index < userAddressesArrayLength; index++) {
+      const userAddress = await drizzle.contracts.OpenHire.methods
+        .allUserAddresses(index)
+        .call();
+      const userStruct = await drizzle.contracts.OpenHire.methods
+        .getUserData(userAddress)
+        .call();
 
-            if (userStruct[2].includes(currentLoggedInAddress)) {
-                const CURRENT_EMPLOYEE_INFO = {
-                    name: userStruct[0],
-                    emailAddress: userStruct[1],
-                    ethereumAddress: userAddress
-                };
-                CURRENT_EMPLOYEES.push(CURRENT_EMPLOYEE_INFO)
-            }
+      if (userStruct[2].includes(currentLoggedInAddress)) {
+        const experienceLength = await drizzle.contracts.OpenHire.methods
+          .getExperienceLength(userAddress)
+          .call();
+
+        for (let expIndex = 0; expIndex < experienceLength; expIndex++) {
+          const experience = await drizzle.contracts.OpenHire.methods
+            .getExperience(userAddress, expIndex)
+            .call();
+          console.log(userAddress, experience);
+          console.log(experience[2] == new Date().getFullYear());
+          if (experience[2].toString() == new Date().getFullYear()) {
+            const CURRENT_EMPLOYEE_INFO = {
+              name: userStruct[0],
+              emailAddress: userStruct[1],
+              ethereumAddress: userAddress
+            };
+            CURRENT_EMPLOYEES.push(CURRENT_EMPLOYEE_INFO);
+          }
         }
-        this.setState({
-            CURRENT_EMPLOYEES
-        })
-
-
+      }
     }
+    this.setState({
+      CURRENT_EMPLOYEES
+    });
+  }
 
-    render() {
-        // fetch all users
-        // filter users by matching org address in experience
-        // have side toggle bar
-        // check all users for a pending approval
-        let currentEmployees;
-        if (this.state.CURRENT_EMPLOYEES) {
-            currentEmployees = this.state.CURRENT_EMPLOYEES.map(
-                (employee, index) => {
-                    return <li key={index}>{employee.name}, {employee.emailAddress}, {employee.ethereumAddress}</li>
-                }
-            )
-        }
+  render() {
+    console.log("this.props", this.props);
+    // fetch all users
+    // filter users by matching org address in experience
+    // have side toggle bar
+    // check all users for a pending approval
+    let currentEmployees;
+    if (this.state.CURRENT_EMPLOYEES) {
+      currentEmployees = this.state.CURRENT_EMPLOYEES.map((employee, index) => {
         return (
-            <React.Fragment>
-                <hr></hr>
-                <h1>Current Employees</h1>
-                <div>
-                    {currentEmployees}
-                </div>
-            </React.Fragment>
-        )
+          <Card
+            href={`/user/${employee.ethereumAddress}`}
+            header={employee.name}
+            description={employee.emailAddress}
+            meta={employee.ethereumAddress}
+            color="teal"
+            className="orgCard"
+            style={{
+              width: "500px"
+            }}
+          />
+          //   <li key={index}>
+          // {employee.name}, {employee.emailAddress}, {employee.ethereumAddress}
+          //   </li>
+        );
+      });
     }
+    return (
+      <React.Fragment>
+        <hr />
+        <h1>Current Employees</h1>
+        <div>{currentEmployees}</div>
+      </React.Fragment>
+    );
+  }
 }
