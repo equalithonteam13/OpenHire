68137da248ddda288bad8cf2a4abcdc094f42423
Sofia Benitez <sofia.benitez39@gmail.com>
2019-08-08 16:39:17 -0400

    fixing sticky footer

--- openhire/src/SignupForm.js
+++ openhire/src/SignupForm.js
@@ -3,119 +3,113 @@ import { toast, Flip } from 'react-toastify';
 import { Button, Form, Input, Checkbox, Label, Icon } from 'semantic-ui-react';
 
 export default class SignupForm extends Component {
-  constructor() {
-    super();
-    this.state = {
-      currentAddress: '',
-      name: '',
-      email: '',
-      organizationForm: false,
-      loading: false,
-      errorMessage: '',
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			currentAddress: '',
+			name: '',
+			email: '',
+			organizationForm: false,
+			loading: false,
+			errorMessage: ''
+		};
+	}
 
-  async componentDidMount() {
-    const currentAddress = await this.props.drizzle.web3.eth.getAccounts();
-    this.setState({
-      currentAddress: currentAddress[0],
-    });
-  }
+	async componentDidMount() {
+		const currentAddress = await this.props.drizzle.web3.eth.getAccounts();
+		this.setState({
+			currentAddress: currentAddress[0]
+		});
+	}
 
-  handleInputChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
+	handleInputChange = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	};
 
-  handleSubmit = async event => {
-    event.preventDefault();
-    this.setState({ loading: true });
+	handleSubmit = async (event) => {
+		event.preventDefault();
+		this.setState({ loading: true });
 
-    toast.info('Processing sign up...', {
-      position: 'top-right',
-      autoClose: 10000,
-      transition: Flip,
-    });
-    try {
-      if (this.state.organizationForm) {
-        await this.props.drizzle.contracts.OpenHire.methods
-          .createOrganization(this.state.name, this.state.email)
-          .send({ from: this.state.currentAddress });
-      } else {
-        await this.props.drizzle.contracts.OpenHire.methods
-          .createUser(this.state.name, this.state.email)
-          .send({ from: this.state.currentAddress });
-      }
-      this.setState({ errorMessage: '' });
-    } catch (error) {
-      toast.dismiss();
-      this.setState({ errorMessage: error.message });
-    }
-    this.setState({ loading: false, name: '', email: '' });
-  };
+		toast.info('Processing sign up...', {
+			position: 'top-right',
+			autoClose: 10000,
+			transition: Flip
+		});
+		try {
+			if (this.state.organizationForm) {
+				await this.props.drizzle.contracts.OpenHire.methods
+					.createOrganization(this.state.name, this.state.email)
+					.send({ from: this.state.currentAddress });
+			} else {
+				await this.props.drizzle.contracts.OpenHire.methods
+					.createUser(this.state.name, this.state.email)
+					.send({ from: this.state.currentAddress });
+			}
+			this.setState({ errorMessage: '' });
+		} catch (error) {
+			toast.dismiss();
+			this.setState({ errorMessage: error.message });
+		}
+		this.setState({ loading: false, name: '', email: '' });
+	};
 
-  toggleForm = option => {
-    if (option !== undefined) {
-      this.setState({ organizationForm: option });
-    } else {
-      this.setState(prevState => ({
-        organizationForm: !prevState.organizationForm,
-      }));
-    }
-  };
+	toggleForm = (option) => {
+		if (option !== undefined) {
+			this.setState({ organizationForm: option });
+		} else {
+			this.setState((prevState) => ({
+				organizationForm: !prevState.organizationForm
+			}));
+		}
+	};
 
-  render() {
-    return (
-      <div>
-        <h1>Form</h1>
-        <Form onSubmit={this.handleSubmit}>
-          <Label
-            as="a"
-            image
-            onClick={() => this.toggleForm(false)}
-            color={this.state.organizationForm ? 'grey' : 'blue'}
-          >
-            <Icon name="user" />
-            User
-          </Label>
-          <Checkbox
-            toggle
-            checked={this.state.organizationForm}
-            onClick={() => this.toggleForm()}
-          />
-          <Label
-            as="a"
-            onClick={() => this.toggleForm(true)}
-            color={this.state.organizationForm ? 'blue' : 'grey'}
-          >
-            <Icon name="building" />
-            Organization
-          </Label>
-          <Input
-            key="name"
-            name="name"
-            value={this.state.name}
-            placeholder="Name"
-            onChange={this.handleInputChange}
-          />
-          <Input
-            key="email"
-            name="email"
-            value={this.state.email}
-            placeholder="Email Address"
-            type="email"
-            onChange={this.handleInputChange}
-          />
-          <Button
-            type="submit"
-            disabled={this.state.loading}
-            loading={this.state.loading}
-          >
-            Submit
-          </Button>
-        </Form>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<h1>Form</h1>
+				<div className="signup-content">
+					<Form onSubmit={this.handleSubmit}>
+						<Label
+							as="a"
+							image
+							onClick={() => this.toggleForm(false)}
+							color={this.state.organizationForm ? 'grey' : 'blue'}
+						>
+							<Icon name="user" />
+							User
+						</Label>
+						<Checkbox toggle checked={this.state.organizationForm} onClick={() => this.toggleForm()} />
+						<Label
+							as="a"
+							onClick={() => this.toggleForm(true)}
+							color={this.state.organizationForm ? 'blue' : 'grey'}
+						>
+							<Icon name="building" />
+							Organization
+						</Label>
+						<Input
+							key="name"
+							name="name"
+							value={this.state.name}
+							placeholder="Name"
+							onChange={this.handleInputChange}
+						/>
+						<Input
+							key="email"
+							name="email"
+							value={this.state.email}
+							placeholder="Email Address"
+							type="email"
+							onChange={this.handleInputChange}
+						/>
+						<Button type="submit" disabled={this.state.loading} loading={this.state.loading}>
+							Submit
+						</Button>
+					</Form>
+				</div>
+			</div>
+		);
+	}
 }
--- openhire/src/SplashPage.js
+++ openhire/src/SplashPage.js
@@ -1,102 +1,93 @@
-import React from "react";
-import {
-  Button,
-  Divider,
-  Grid,
-  Header,
-  Icon,
-  Segment
-} from "semantic-ui-react";
-import { withRouter } from "react-router-dom";
+import React from 'react';
+import { Button, Divider, Grid, Header, Icon, Segment } from 'semantic-ui-react';
+import { withRouter } from 'react-router-dom';
 
-import SearchBar from "./SearchBar";
+import SearchBar from './SearchBar';
 
 class SplashPage extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      loggedIn: false
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			loggedIn: false
+		};
+	}
 
-  async componentDidMount() {
-    const { drizzle, drizzleState } = this.props;
-    const user = await drizzle.contracts.OpenHire.methods
-      .getUserData(drizzleState.accounts[0])
-      .call();
-    if (user[0]) {
-      this.setState({
-        loggedIn: true
-      });
-    }
-  }
+	async componentDidMount() {
+		const { drizzle, drizzleState } = this.props;
+		const user = await drizzle.contracts.OpenHire.methods.getUserData(drizzleState.accounts[0]).call();
+		if (user[0]) {
+			this.setState({
+				loggedIn: true
+			});
+		}
+	}
 
-  render() {
-    const { drizzle, drizzleState, props } = this.props;
-    const { loggedIn } = this.state;
-    return (
-      <div>
-        <Segment placeholder>
-          <Grid columns={2} stackable textAlign="center">
-            <Divider vertical>Or</Divider>
+	render() {
+		const { drizzle, drizzleState, props } = this.props;
+		const { loggedIn } = this.state;
+		return (
+			<div>
+				<Segment placeholder>
+					<Grid columns={2} stackable textAlign="center">
+						<Divider vertical>Or</Divider>
 
-            <Grid.Row verticalAlign="middle">
-              <Grid.Column>
-                <Header icon style={{ margin: 0 }}>
-                  <Icon name="search" />
-                  Find Profile
-                </Header>
-                <SearchBar
-                  drizzle={drizzle}
-                  drizzleState={drizzleState}
-                  placeholder={"Search profiles..."}
-                />
-              </Grid.Column>
-              {loggedIn ? (
-                <Grid.Column>
-                  <Header icon style={{ margin: 0 }}>
-                    <div style={{ display: "flex" }}>
-                      <Icon name="user" />
-                      <Icon name="building" />
-                    </div>
-                    <Header.Content>Visit Profile</Header.Content>
-                  </Header>
+						<Grid.Row verticalAlign="middle">
+							<Grid.Column>
+								<Header icon style={{ margin: 0 }}>
+									<Icon name="search" />
+									Find Profile
+								</Header>
+								<SearchBar
+									drizzle={drizzle}
+									drizzleState={drizzleState}
+									placeholder={'Search profiles...'}
+								/>
+							</Grid.Column>
+							{loggedIn ? (
+								<Grid.Column>
+									<Header icon style={{ margin: 0 }}>
+										<div style={{ display: 'flex' }}>
+											<Icon name="user" />
+											<Icon name="building" />
+										</div>
+										<Header.Content>Visit Profile</Header.Content>
+									</Header>
 
-                  <Button
-                    primary
-                    onClick={() => {
-                      props.history.push(`/user/${drizzleState.accounts[0]}`);
-                    }}
-                  >
-                    Profile
-                  </Button>
-                </Grid.Column>
-              ) : (
-                <Grid.Column>
-                  <Header icon style={{ margin: 0 }}>
-                    <div style={{ display: "flex" }}>
-                      <Icon name="user" />
-                      <Icon name="building" />
-                    </div>
-                    <Header.Content>Create New Profile</Header.Content>
-                  </Header>
+									<Button
+										primary
+										onClick={() => {
+											props.history.push(`/user/${drizzleState.accounts[0]}`);
+										}}
+									>
+										Profile
+									</Button>
+								</Grid.Column>
+							) : (
+								<Grid.Column>
+									<Header icon style={{ margin: 0 }}>
+										<div style={{ display: 'flex' }}>
+											<Icon name="user" />
+											<Icon name="building" />
+										</div>
+										<Header.Content>Create New Profile</Header.Content>
+									</Header>
 
-                  <Button
-                    primary
-                    onClick={() => {
-                      props.history.push("/signup");
-                    }}
-                  >
-                    Sign Up
-                  </Button>
-                </Grid.Column>
-              )}
-            </Grid.Row>
-          </Grid>
-        </Segment>
-      </div>
-    );
-  }
+									<Button
+										primary
+										onClick={() => {
+											props.history.push('/signup');
+										}}
+									>
+										Sign Up
+									</Button>
+								</Grid.Column>
+							)}
+						</Grid.Row>
+					</Grid>
+				</Segment>
+			</div>
+		);
+	}
 }
 
 export default withRouter(SplashPage);
